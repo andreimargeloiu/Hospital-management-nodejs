ed4fd23d3481957208a643ce5b983d5cfe41a09b
Gleb Skryabin <Gleb@eduroam-int-dhcp-98-44-196.ucl.ac.uk>
2017-03-12 14:27:11 +0000

    The stuff no backgroun image

---
+++ public/js/patientView.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
